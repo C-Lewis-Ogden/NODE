3d43fb1e51147b16007ef28242561b5b2eef0dac
C-Lewis-Ogden <70986647+C-Lewis-Ogden@users.noreply.github.com>
2021-12-08 21:25:03 -0500

    Update run.py
    This is the script for running the Port Scanning feature in the ThreatWare NODE.

--- modules/portscan/run.py
+++ modules/portscan/run.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 import socket, threading, sys, os, re, json
 
+# Open the Config.JSON file that contains some initializing information for the script
 f = open('config.json')
 config = json.load(f)
 f.close()
 
+# Taking the IP address to scan from the Config.JSON file
+# By default, this is set to scan itself using 127.0.0.1 as an address
 ip = config['address'] 
 threads = []
 open_ports = {}
 
+# Creating the HTML table data
 table_header_html="<tr><th>Open Ports</th></tr>"
 table_data_html=""
 
+# A function that accepts an IP address, a port number, and an array of ports
+# This function will open a socket and attempt a connection to the provided IP on the provided port
+# The result of the connection attempt is stored in the array of ports
 def try_port(ip, port, open_ports):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,6 +33,10 @@ def try_port(ip, port, open_ports):
         open_ports[port] = 'closed'
         return None
 
+# A function that accepts an IP address
+# This function will create a thread using the try_port function and add it to an array of threads
+# It then loops through the range of ports provided, starting each thread
+# The array of ports is the checked and any open ports are added to the HTML table
 def scan_ports(ip):
     
     table_data=""
@@ -46,6 +57,7 @@ def scan_ports(ip):
             
     return table_data
 
+# Calling the scan_port function to run the above code
 try:
     table_data_html = scan_ports(ip)
     
@@ -58,13 +70,15 @@ except socket.gaierror:
 except socket.error:
     sys.exit()
 
+# Create the HTML statement for the output
 html_output = "<table>%s%s</table>"%(table_header_html,table_data_html)
 
+# Check for the next available number in the log file naming convention
 log_count = 1
 while os.path.exists("output_%s.log"%log_count):
-    log_count += 1
-# The script when run using the API seems to fail at or during the next line
-# This doesn't appear to be an issue when run from the Node's console   
+    log_count += 1  
+
+# Open the log file and write the HTML statement to the log file and console
 f = open('output_%s.log'%log_count,'w')
 f.write(html_output)
 f.close()
